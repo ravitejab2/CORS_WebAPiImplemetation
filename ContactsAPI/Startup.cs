87270fd389289de1c1f14272d03c36b5eab51769
ravitejabollineni555@gmail.com <ravitejabollineni555@gmail.com>
2022-09-02 16:33:54 +0530

    Add project files.

---
+++ ContactsAPI/Startup.cs
@@ -0,0 +1,61 @@
+using ContactsLib.Models;
+using ContactsLib.Models.Repositories;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+namespace ContactsAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddScoped<IContactsRepo, ContactsRepo>();
+            services.AddScoped<ICitiesRepo, CitiesRepo>();
+            services.AddDbContext<WebApiCoreAssignmentContext>(x => x.UseSqlServer(Configuration.GetConnectionString("DBConnection")));
+            services.AddCors(c =>
+            {
+                c.AddPolicy("AllowOrigin", options => options.WithOrigins("https://localhost:44390"));
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+            app.UseCors();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
---
+++ ContactsLib_Lab3/Models/Contact.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+#nullable disable
+
+namespace ContactsLib.Models
+{
+    public partial class Contact
+    {
+        public int ContactNo { get; set; }
+        [RegularExpression("^[a-zA-Z ]*$", ErrorMessage = "Contact name should not contain numbers")]
+        [Required]
+        public string ContactName { get; set; }
+
+        [Required]
+        [ValidateCityName(ErrorMessage = "CityName is not available")]
+        public string CityName { get; set; }
+        [Required]
+
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Cell no must contain only numbers")]
+        public long CellNo { get; set; }
+    }
+}
